fb51074304753d18c6b80333f969a4f86424f725
Mohit Mamoria <mohit.mamoria@gmail.com>
2015-09-23 18:41:26 +0530

    updated berksfile and metadata

--- metadata.rb
+++ metadata.rb
@@ -1,8 +1,9 @@
 name             'deploy-apps'
-maintainer       'YOUR_NAME'
-maintainer_email 'YOUR_EMAIL'
+maintainer       'Mohit Mamoria'
+maintainer_email 'mohit.mamoria@gmail.com'
 license          'All rights reserved'
 description      'Installs/Configures deploy-apps'
 long_description 'Installs/Configures deploy-apps'
 version          '0.1.0'
+depends          'composer', '~> 1.0.5'
 
